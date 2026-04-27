69572666f64f06343584b44588d76f26ecfcfddb
Seungmin Nam <nhjbest22@g.skku.edu>
2026-04-27 22:52:16 +0900

    [Gold II] Title: 우수 마을, Time: 4 ms, Memory: 3468 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.cc
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+
+// 북 서 동 남
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+
+ll POW(ll a, ll b, ll MMM) {
+  ll ret = 1;
+  for (; b; b >>= 1, a = (a * a) % MMM)
+    if (b & 1) ret = (ret * a) % MMM;
+  return ret;
+}
+ll GCD(ll a, ll b) { return b ? GCD(b, a % b) : a; }
+ll LCM(ll a, ll b) {
+  if (a == 0 || b == 0) return a + b;
+  return a / GCD(a, b) * b;
+}
+ll INV(ll a, ll m) {
+  ll m0 = m, y = 0, x = 1;
+  if (m == 1) return 0;
+  while (a > 1) {
+    ll q = a / m;
+    ll t = m;
+    m = a % m, a = t;
+    t = y;
+    y = x - q * y;
+    x = t;
+  }
+  if (x < 0) x += m0;
+  return x;
+}
+pll EXGCD(ll a, ll b) {
+  if (b == 0) return {1, 0};
+  auto t = EXGCD(b, a % b);
+  return {t.second, t.first - t.second * (a / b)};
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define visit VISIT
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f)                                                \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' \
+       << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f)                                               \
+  cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' \
+       << (f) << '\n'
+#define pfvec(V) \
+  for (auto const& t : V) pf1(t)
+#define pfvecl(V)                 \
+  for (auto const& t : V) pf1(t); \
+  pf0l()
+#define debug(x) cout << #x << " is " << x << '\n';
+#define endl '\n'
+
+int N;
+vector<int> adj[10'005];
+int people[10'005];
+int parent[10'005];
+int dp[10'005][2];
+
+int ans;
+
+void dfs(int par, int cur) {
+  parent[cur] = par;
+
+  for (auto nxt_idx : adj[cur]) {
+    if (nxt_idx == par) continue;
+
+    dfs(cur, nxt_idx);
+  }
+}
+
+int solve(int par, int cur, bool cur_state) {
+  if (dp[cur][cur_state] != -1) return dp[cur][cur_state];
+
+  dp[cur][cur_state] = cur_state ? people[cur] : 0;
+
+  for (auto nxt_idx : adj[cur]) {
+    if (nxt_idx == par) continue;
+
+    if (cur_state) {
+      dp[cur][cur_state] += solve(cur, nxt_idx, false);
+      continue;
+    }
+
+    dp[cur][cur_state] +=
+        max(solve(cur, nxt_idx, false), solve(cur, nxt_idx, true));
+
+    // 내가 1
+    // 부모가 0
+    // 자식이 0
+
+    // 내가 0
+    // 부모가 0 아니면 1
+    // 1. 부모가 0 -> 무조건 1
+    // 2. 부모가 1 -> 0 or 1
+
+    // 다만 dp에 의해 최댓값만 가져가므로 어차피 부모 0, 자식 0 이면서 자식이 리프 노드인 케이스는 자연스럽게 걸러짐
+  }
+
+  return dp[cur][cur_state];
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  fill(&dp[0][0], &dp[0][0] + 10'005 * 2, -1);
+
+  cin >> N;
+
+  rep(i, 0, N) cin >> people[i + 1];
+  rep(i, 0, N - 1) {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  dfs(0, 1);
+
+  solve(0, 1, true);
+  solve(0, 1, false);
+
+  cout << max(dp[1][true], dp[1][false]);
+
+  return 0;
+}
