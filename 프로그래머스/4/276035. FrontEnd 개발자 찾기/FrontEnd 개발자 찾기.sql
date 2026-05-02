c7526289c64d8ebc901caef44e37428f24f003a6
Seungmin Nam <nhjbest22@g.skku.edu>
2026-05-02 20:12:27 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,18 @@
+SELECT
+    D.ID,
+    D.EMAIL,
+    D.FIRST_NAME,
+    D.LAST_NAME
+FROM
+    DEVELOPERS D
+WHERE
+    D.SKILL_CODE & (
+        SELECT
+            SUM(CODE)
+        FROM
+            SKILLCODES
+        WHERE
+            CATEGORY = 'Front End'
+    )
+ORDER BY
+    D.ID ASC;
