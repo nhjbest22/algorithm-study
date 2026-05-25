022f588c060d638547f9624add260cc7b7b68103
Seungmin Nam <nhjbest22@g.skku.edu>
2026-05-26 01:04:14 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 268.41 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    stack<pair<int, int>> s;
     vector<int> res(numbers.size(), -1);
     
     for(int i = 0; i < numbers.size(); i++){
-        while(!pq.empty()){
-            auto cur = pq.top();
+        while(!s.empty()){
+            auto cur = s.top();
             if(cur.first >= numbers[i]) break;
             
-            pq.pop();
+            s.pop();
             
             res[cur.second] = numbers[i];
         }
         
-        pq.push({numbers[i], i});
+        s.push({numbers[i], i});
     }
     
     return res;
