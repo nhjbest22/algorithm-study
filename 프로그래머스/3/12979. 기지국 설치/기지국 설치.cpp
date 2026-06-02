f2548bae7e2cc38e6872dee7f271f5500bb329cd
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-02 14:20:52 +0900

    [level 3] Title: 기지국 설치, Time: 0.08 ms, Memory: 3.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -6,43 +6,26 @@ int solution(int n, vector<int> stations, int w)
 {
     int ans = 0;
     // 1 ~ N까지 커버해야 함
-    
-    vector<pair<int, int>> grid, grid_merge;
-    
-    for(auto& idx: stations){
-        int st = max(1, idx - w);
-        int en = min(n, idx + w);
-        
-        if(grid.empty()) {
-           grid.push_back({st, en});
-            continue;
-        }
-        
-        if(grid.back().second + 1 < st){
-            grid.push_back({st, en});
-            continue;
-        }
-            
-        
-        auto& G = grid.back();
-        G.second = en;
-    }
-    
     int cur = 1;
     int len = 2*w + 1;
-    for(auto& G: grid){
-        auto& [st, en] = G;
     
+    for(auto& idx: stations){
+        int st = idx - w;
+        int en = idx + w;
         
-        ans += (st - cur)/len;
-        ans += ((st - cur) % len > 0);
+        if(st <= cur){
+            cur = en + 1;
+            continue;
+        }
         
+        int gap = st - cur;
+        ans += (gap / len) + (gap % len > 0);
         cur = en + 1;
     }
     
     if(cur <= n){
         ans += (n - cur + 1) / len;
-        ans += ((n - cur + 1) % len > 0);
+        ans += (n - cur + 1) % len > 0;
     }
 
     return ans;
