59fbd00afc78d675e37782ec2dbf606ec581272f
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-03 11:55:50 +0900

    Time: 0 ms (100%), Space: 20.8 MB (49.58%) - LeetHub

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,30 +6,27 @@ private:
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int M = matrix.size(), N = matrix[0].size();
-        vector<pair<int, int>> grid;
+        vector<int> row(M, 0), col(N, 0);
 
 
         for(int i = 0; i < M; i++){
             for(int j = 0; j < N; j++){
-                if(!matrix[i][j]) grid.push_back({i, j});
+                if(matrix[i][j]) continue;
+
+                row[i] = col[j] = 1;
             }
         }
 
-        for(auto& p: grid){
-            for(int dir = 0; dir< 4; dir++){
-                int len = 1;
-                while(1){
-                    int x = p.first + len*dx[dir];
-                    int y = p.second + len*dy[dir];
+        for(int i = 0; i < M; i++){
+            if(!row[i]) continue;
 
-                    len++;
+            for(int j = 0; j < N; j++) matrix[i][j] = 0;
+        }
 
-                    if(x < 0 || x >= M || y < 0 || y>= N) break;
-                    if(!matrix[x][y]) continue;
+        for(int j = 0; j < N; j++){
+            if(!col[j]) continue;
 
-                    matrix[x][y] = 0;
-                }
-            }
+            for(int i = 0; i < M; i++) matrix[i][j] = 0;
         }
     }
 };
