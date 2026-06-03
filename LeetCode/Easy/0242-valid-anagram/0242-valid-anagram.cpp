079a82a24e3933fb465d533d1d322112dae56b83
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-03 12:02:15 +0900

    Time: 0 ms (100%), Space: 9.7 MB (48.74%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+    int cnt1['z' - 'a' + 1];
+    int cnt2['z' - 'a' + 1];
 
-        return s == t;
+    bool isAnagram(string s, string t) {
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        fill(cnt1, cnt1 + ('z' - 'a' + 1), 0);
+        fill(cnt2, cnt2 + ('z' - 'a' + 1), 0);
+
+        for(auto& ch: s){
+            int idx = ch - 'a';
+            cnt1[idx]++;
+        }
+
+        for(auto& ch: t){
+            int idx = ch - 'a';
+            cnt2[idx]++;
+        }
+
+        for(int i = 0; i < 'z' - 'a' + 1; i++){
+            if(cnt1[i] != cnt2[i]) return false;
+        }
+
+        return true;
     }
 };
