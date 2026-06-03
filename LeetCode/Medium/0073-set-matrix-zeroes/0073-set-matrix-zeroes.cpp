5dd2fa71d5acf3e84a1905bf6936472f4cfb9cf6
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-03 11:18:46 +0900

    Time: 75 ms (5.24%), Space: 21.9 MB (5.11%) - LeetHub

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4]= {0, 1, 0, -1};
+
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int, int>> grid;
+
+        int M = matrix.size(), N = matrix[0].size();
+
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(!matrix[i][j]) grid.push_back({i, j});
+            }
+        }
+
+        for(auto& p: grid){
+            for(int dir = 0; dir< 4; dir++){
+                int len = 1;
+                while(1){
+                    int x = p.first + len*dx[dir];
+                    int y = p.second + len*dy[dir];
+
+                    len++;
+
+                    if(x < 0 || x >= M || y < 0 || y>= N) break;
+                    if(!matrix[x][y]) continue;
+
+                    matrix[x][y] = 0;
+                }
+            }
+        }
+    }
+};
