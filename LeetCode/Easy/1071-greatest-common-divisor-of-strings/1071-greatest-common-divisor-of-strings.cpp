9cf41f3c898289df9c95ff30de56ed4c3525fc83
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-12 22:28:48 +0900

    Time: 2 ms (28.39%), Space: 9.9 MB (32.95%) - LeetHub

--- LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
-    bool isGCD(const string& target, int len){
-        int N = target.size() / len;
-
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < len; j++){
-                if(target[i*len + j] != target[j]) return false;
-            }
-        }
-
-        return true;
-    }
-
     string gcdOfStrings(string str1, string str2) {
-        int N = min(str1.size(), str2.size());
+        if(str1 + str2 != str2 + str1) return "";
 
-        int ans = 0;
-        for(int i = 0; i < N; i++){
-            if(str1[i] != str2[i]) break;
-            if(str1.size() % (i+1) != 0) continue;
-            if(str2.size() % (i+1) != 0) continue;
-
-            if(isGCD(str1, i+1) && isGCD(str2, i+1)) ans = i+1;
-        }
-
-        return str1.substr(0, ans);
+        return str1.substr(0, __gcd(str1.size(), str2.size()));
     }
 };
