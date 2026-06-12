b1f9a1dc4bc2ba8299fd47c1b521405277cec24c
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-13 01:57:14 +0900

    Time: 0 ms (100%), Space: 97.1 MB (83.17%) - LeetHub

--- LeetCode/Easy/2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ LeetCode/Easy/2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -3,17 +3,18 @@ public:
     int findNonMinOrMax(vector<int>& nums) {
         if(nums.size() <= 2) return -1;
 
-        // int MAX = -1, MIN = 105;
-        // for(int i = 0; i < nums.size(); i++){
-        //     MAX = max(MAX, nums[i]);
-        //     MIN = min(MIN, nums[i]);
-        // }
+        int N = nums.size();
 
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(nums[i] != MAX && nums[i] != MIN) return nums[i];
-        // }
+        int MAX = -1, MIN = 105;
+        for(int i = 0; i < N; i++){
+            MAX = max(MAX, nums[i]);
+            MIN = min(MIN, nums[i]);
+        }
 
-        sort(nums.begin(), nums.end());
-        return nums[1];
+        for(int i = 0; i < N; i++){
+            if(nums[i] != MAX && nums[i] != MIN) return nums[i];
+        }
+
+        return 0;
     }
 };
