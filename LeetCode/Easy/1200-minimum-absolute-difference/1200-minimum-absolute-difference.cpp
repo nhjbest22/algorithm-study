8b22e0c8b7c2ba453ef1a5113d07dee424221653
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-12 21:21:36 +0900

    Time: 15 ms (75.97%), Space: 37.3 MB (9.34%) - LeetHub

---
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>> ans;
+
+        sort(arr.begin(), arr.end());
+        int MIN = INT32_MAX;
+        for(int i = 1; i< arr.size(); i++){
+            int diff = arr[i] - arr[i-1];
+            MIN = min(MIN, diff);
+        }
+
+        for(int i = 1; i< arr.size(); i++){
+            int diff = arr[i] - arr[i-1];
+
+            if(diff != MIN) continue;
+
+            vector<int> v = {arr[i-1], arr[i]};
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
