5dbe660b94c49adba08a174c489f4c72b83975bb
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-17 11:35:39 +0900

    Time: 0 ms (100%), Space: 29.9 MB (30.2%) - LeetHub

---
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(!k) return;
+
+        int N = nums.size();
+
+
+
+        int buffer[N+10];
+        int st = N - (k%N);
+
+        st %= N;
+
+        for(int i = 0;i < N; i++){
+            buffer[i] = nums[st++];
+
+            st %= N;
+        }
+
+        for(int i = 0;i < N; i++){
+            nums[i] = buffer[i];
+        }
+    }
+};
