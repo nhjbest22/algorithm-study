a3e25c019d0b8e49d8d05d9ed662677e0c22b391
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-19 03:30:35 +0900

    Time: 2 ms (24.95%), Space: 9 MB (58.06%) - LeetHub

--- LeetCode/Easy/0067-add-binary/0067-add-binary.cpp
+++ LeetCode/Easy/0067-add-binary/0067-add-binary.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-
         string ans = "";
 
-        int l = 0, r = 0;
-        int overSum = 0;
-        while(l < a.size() && r < b.size()){
-            int sum = (a[l++] - '0') + (b[r++] - '0') + overSum;
+        int l = a.size() -1, r = b.size() - 1;
+        int carry = 0;
+
+        while(l >= 0 || r >= 0 || carry != 0){
+            int sum = 0;
+            if(l >= 0)
+                sum += a[l--] - '0';
             
-            overSum = sum >= 2;
-            ans += to_string((sum % 2));
-        }
+            if(r >= 0)
+                sum += b[r--] - '0';
 
-        while(l < a.size()){
-            int sum = (a[l++] - '0') + overSum;
-            overSum = sum >= 2;
-            ans += to_string((sum % 2));
-        }
+            sum += carry;
+            ans += (sum%2) + '0';
 
-        while(r < b.size()){
-            int sum = (b[r++] - '0') + overSum;
-            overSum = sum >= 2;
-            ans += to_string((sum % 2));
+            carry = sum >= 2;
         }
-
-        if(overSum) ans += '1';
 
         reverse(ans.begin(), ans.end());
 
