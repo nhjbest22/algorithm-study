b5849f8f2d42473760f1283bf3efdf6f3ae9a814
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-21 00:09:01 +0900

    Time: 41 ms (59.59%), Space: 22.4 MB (77.14%) - LeetHub

---
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int N = s.size();
+        unordered_set<string> us1, us2;
+
+        for(int i = 0; i < N; i++){
+            if(i + 10 > N) break;
+
+            string str = s.substr(i, 10);
+            if(us1.find(str) != us1.end())
+                us2.insert(str);
+            
+            us1.insert(str);
+        }
+
+        vector<string> ans(us2.begin(), us2.end());
+        return ans;
+    }
+};
