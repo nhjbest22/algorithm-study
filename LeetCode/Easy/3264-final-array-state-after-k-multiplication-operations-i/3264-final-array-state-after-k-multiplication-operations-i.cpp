c34c1a7ff68df8df5ce64fd996e7ab614abe6d3a
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-23 02:32:18 +0900

    Time: 3 ms (32.97%), Space: 27.8 MB (25.41%) - LeetHub

--- LeetCode/Easy/3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ LeetCode/Easy/3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        for(int i = 0; i < nums.size(); i++){
+            pq.push({nums[i], i});
+        }
+
         while(k--){
-            auto it = min_element(nums.begin(), nums.end());
-            *it *= multiplier;
+            auto [cur, i] = pq.top();
+            pq.pop();
+
+            cur *= multiplier;
+            
+            nums[i] = cur;
+            pq.push({cur, i});
         }
         return nums;
     }
