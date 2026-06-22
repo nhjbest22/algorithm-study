2fab288a74b6f3b9cb4e85d397170de564863a0a
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-23 03:17:07 +0900

    Time: 4 ms (71.43%), Space: 28.5 MB (97.14%) - LeetHub

--- LeetCode/Medium/3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
+++ LeetCode/Medium/3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
@@ -13,27 +13,19 @@ public:
             for(auto& ch: word) cnt[ch - 'a']++;
         }
 
-        int one = 0, two = 0;
+        int total_pair = 0;
         for(int i = 0; i < MAX; i++){
-            two += cnt[i]/2;
-            one += cnt[i]%2;
+            total_pair += cnt[i]/2;
         }
 
         sort(word_len.begin(), word_len.end());
 
         for(auto& len: word_len){
-            int one_cnt = len%2;
-            int two_cnt = len/2;
-
-            if(two_cnt > two) continue;
+            int needed_pair = len/2;
+            if(needed_pair > total_pair) break;
 
             ans++;
-            if(one_cnt){
-                two = one ? two : two - 1;
-                one = one ? one -1 : one + 1;
-            }
-
-            two -= two_cnt;
+            total_pair -= needed_pair;
         }
 
         return ans;
