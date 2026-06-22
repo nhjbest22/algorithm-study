e8f6e6721186467786e2b81195779cba4fdaf43b
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-23 00:58:08 +0900

    Time: 18 ms (49.69%), Space: 24.5 MB (62.08%) - LeetHub

--- LeetCode/Medium/1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ LeetCode/Medium/1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
+    const int dx[4] = {-1, 0, 1, 0};
+    const int dy[4] = {0, 1, 0, -1};
+
     int maxDistance(vector<vector<int>>& grid) {
-        vector<pair<int, int>> lands;
+        // 0 -> 물, 1 -> 땅
+
+        int cost[105][105];
+        fill(&cost[0][0], &cost[0][0] + 105*105, -1);
+        
         int N = grid.size();
         int MAX = -1;
+
+        queue<pair<int, int>> Q;
         
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 if(grid[i][j] == 0) continue;
-
-                lands.push_back({i, j});
+                
+                cost[i][j] = 0;
+                Q.push({i, j});
             }
         }
 
-        if(lands.size() == 0) return -1;
+        while(!Q.empty()){
+            auto cur = Q.front();
+            Q.pop();
+
+            for(int dir = 0; dir < 4; dir++){
+                int x = cur.first + dx[dir];
+                int y = cur.second + dy[dir];
+
+                if(x < 0 || x >= N || y < 0 || y >= N) continue;
+                if(grid[x][y] == 1) continue;
+                if(cost[x][y] != -1) continue;
+
+                cost[x][y] = cost[cur.first][cur.second] + 1;
+                Q.push({x, y});
+            }
+        }
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 if(grid[i][j] == 1) continue;
 
-                int dist = INT32_MAX;
-                for(auto& land: lands){
-                    int cur_dist = abs(i - land.first) + abs(j - land.second);
-                    dist = min(dist, cur_dist);
-                }
-
-                MAX = max(dist, MAX);
+                MAX = max(MAX, cost[i][j]);
             }
         }
+
 
         return MAX;
     }
