2958eb3b97a3d9594ac673d950f97f0370450d86
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-23 02:18:42 +0900

    Time: 6 ms (31.03%), Space: 12.6 MB (22.56%) - LeetHub

--- LeetCode/Easy/1184-distance-between-bus-stops/1184-distance-between-bus-stops.cpp
+++ LeetCode/Easy/1184-distance-between-bus-stops/1184-distance-between-bus-stops.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-        int total = accumulate(distance.begin(), distance.end(), 0);
-        
         if(start > destination) swap(start, destination);
         
+        int total = accumulate(distance.begin(), distance.end(), 0);
         int dist = accumulate(distance.begin()+ start, distance.begin() + destination, 0);
 
         return min(total - dist, dist);
