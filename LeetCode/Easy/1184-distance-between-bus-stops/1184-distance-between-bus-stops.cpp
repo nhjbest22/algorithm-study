1dff32749f94ff01ee139302fdd49183aaf8c263
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-23 02:24:46 +0900

    Time: 0 ms (100%), Space: 12.6 MB (22.56%) - LeetHub

--- LeetCode/Easy/1184-distance-between-bus-stops/1184-distance-between-bus-stops.cpp
+++ LeetCode/Easy/1184-distance-between-bus-stops/1184-distance-between-bus-stops.cpp
@@ -5,8 +5,8 @@ public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
         if(start > destination) swap(start, destination);
         
-        int total = reduce(execution::par_unseq, distance.begin(), distance.end(), 0);
-        int dist = reduce(execution::par_unseq, distance.begin()+ start, distance.begin() + destination, 0);
+        int total = reduce(distance.begin(), distance.end(), 0);
+        int dist = reduce(distance.begin()+ start, distance.begin() + destination, 0);
 
         return min(total - dist, dist);
     }
