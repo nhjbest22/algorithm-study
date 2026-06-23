4c2127b8c192309a6c588e72ba344db571cd8698
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-24 01:23:15 +0900

    Time: 8 ms (72.41%), Space: 19.5 MB (83.47%) - LeetHub

--- LeetCode/Easy/1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ LeetCode/Easy/1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -4,11 +4,15 @@ public:
         int ans = 0;
         int N = nums.size();
         
+        int MAX = nums[0];
         for(int i = 1; i < N; i++){
-            if(nums[i] > nums[i-1]) continue;
+            if(MAX < nums[i]){
+                MAX = nums[i];
+                continue;
+            }
 
-            ans += (nums[i-1] - nums[i] + 1);
-            nums[i] = nums[i-1] + 1;
+            ans += (MAX - nums[i] + 1);
+            MAX++;
         }
 
         return ans;
