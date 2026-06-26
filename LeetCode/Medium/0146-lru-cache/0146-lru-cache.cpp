7d3b853c226bb1519d1e25967d63d5f02abeb44f
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-26 11:45:11 +0900

    Time: 52 ms (93.55%), Space: 172.3 MB (95.11%) - LeetHub

---
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,73 @@
+class LRUCache {
+private:
+    int last_update[10'005]; // key to time
+    int time;
+    queue<pair<int ,int>> Q; // key, 시간
+    int data[10'005]; // key to data
+    int capacity;
+
+public:
+    LRUCache(int capacity) {
+        fill(last_update, last_update + 10'005, -1);
+        time = 0;
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        time++;
+
+        if(last_update[key] == -1)
+            return -1;
+
+        Q.push({key, time});
+        last_update[key] = time;
+        
+        return data[key];
+    }
+    
+    void put(int key, int value) {
+        time++;
+        Q.push({key, time});
+
+        if(last_update[key] != -1){
+            last_update[key] = time;
+            data[key] = value;
+
+            return;
+        }
+
+        if(capacity > 0){
+            last_update[key] = time;
+            data[key] = value;
+
+            capacity--;
+
+            return;
+        }
+
+        while(!Q.empty()){
+            auto [cur_key, cur_time] = Q.front();
+            Q.pop();
+
+            if(last_update[cur_key] != cur_time){
+                // 최신 연산이 아님
+                continue;
+            }
+
+            last_update[cur_key] = -1;
+            break;
+        }
+
+        last_update[key] = time;
+        data[key] = value;
+
+        return;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
