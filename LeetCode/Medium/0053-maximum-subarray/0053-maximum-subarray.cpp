2888dd17a5ebc0dca695bb6a6f4fde8467ddf3ab
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-26 11:19:59 +0900

    Time: 2 ms (21.61%), Space: 78.6 MB (14.41%) - LeetHub

---
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> sums;
+        int N = nums.size();
+        
+        int sum = 0;
+        for(int i = 0; i <N; i++){
+            sum += nums[i];
+            sums.push_back(sum);
+        }
+
+        int ans = sums[0];
+        int MIN = min(sums[0], 0);
+
+        for(int i = 1; i < N; i++){
+            // if(MAX < sums[i]){
+            //     MAX = sums[i];
+            //     ans = MAX - MIN;
+            //     continue;
+            // }
+
+            ans = max(ans, sums[i] - MIN);
+
+            MIN = min(MIN, sums[i]);
+        }
+
+        return ans;
+    }
+};
