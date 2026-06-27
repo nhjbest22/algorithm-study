110da3e513144e9f445e6ed82022c4724bd43acd
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-27 20:08:37 +0900

    Time: 0 ms (100%), Space: 23 MB (25.41%) - LeetHub

--- LeetCode/Easy/2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.cpp
+++ LeetCode/Easy/2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int N = nums.size();
-        int cnt = 0;
-
-        bool isCollected[55] = {false, };
+        unordered_set<int> us;
 
         for(int i = N-1; i >=0; i--){
             if(nums[i] > k) continue;
+            
+            us.insert(nums[i]);
 
-            cnt += !isCollected[nums[i]];
-            isCollected[nums[i]] = true;
-
-            if(cnt == k) return N - i;
+            if(us.size() == k) return N - i;
         }
 
         return 0;
