668383afbc667a8c8294fc225485460b19155117
Seungmin Nam <nhjbest22@g.skku.edu>
2026-06-27 19:52:28 +0900

    Time: 52 ms (64.67%), Space: 18.8 MB (38.02%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,8 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int M = matrix.size(), N = matrix[0].size();
         
-        for(int i = 0; i < M; i++){
-            if(binary_search(matrix[i].begin(), matrix[i].end(), target)) return true;
+        int curX = 0, curY = N-1;
+
+        while(curX < M && curY >= 0){
+            auto& cur = matrix[curX][curY];
+            
+            if(cur == target) return true;
+
+            if(cur > target) curY--;
+            else curX++;
         }
 
         return false;
