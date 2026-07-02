5f2e3762202c8e358897dfb4dc5044a7b0b3b793
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-03 02:42:51 +0900

    Time: 0 ms (100%), Space: 10.9 MB (96%) - LeetHub

--- LeetCode/Easy/1122-relative-sort-array/1122-relative-sort-array.cpp
+++ LeetCode/Easy/1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        int st = 0, en = arr1.size() - 1;
-        int N = arr1.size();
-        int isOccured[1'005] = {0, };
-
-        int rank = 1;
+        vector<int> rank(1'005, 2000);
         
+        int R = 0;
         for(auto& num: arr2)
-            isOccured[num] = rank++;
+            rank[num] = R++;
 
-        while(st < en){
-            while(st < N && isOccured[arr1[st]]){
-                st++;
-            }
-
-            while(en >= 0 && !isOccured[arr1[en]]){
-                en--;
-            }
-
-            if(st > en) break;
-
-            swap(arr1[st], arr1[en]);
-        }
-
-        sort(arr1.begin(), arr1.begin() + st, [&](const int& a, const int& b){
-            if(a != b) return isOccured[a] < isOccured[b];
-
-            return false;
+        sort(arr1.begin(), arr1.end(), [&](const int& a, const int& b){
+            if(rank[a] != rank[b]) return rank[a] < rank[b];
+            return a < b;
         });
-        sort(arr1.begin() + st, arr1.end());
 
         return arr1;
     }
