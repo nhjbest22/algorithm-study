29541debad8ebf301e31edbcc648fc28f9546491
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-03 06:58:20 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,25 @@
+WITH O AS (
+    SELECT 
+        * 
+    FROM
+        ANIMAL_OUTS
+    WHERE
+        SEX_UPON_OUTCOME LIKE 'Spayed%'
+    OR
+        SEX_UPON_OUTCOME LIKE 'Neutered%'
+)
+
+SELECT
+    I.ANIMAL_ID,
+    I.ANIMAL_TYPE,
+    I.NAME
+FROM
+    ANIMAL_INS I
+JOIN
+    O
+ON
+    I.ANIMAL_ID = O.ANIMAL_ID
+    AND
+    I.SEX_UPON_INTAKE LIKE 'Intact%'
+ORDER BY
+    I.ANIMAL_ID ASC;
