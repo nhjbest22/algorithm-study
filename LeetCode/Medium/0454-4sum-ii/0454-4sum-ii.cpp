aa575c2d4ff4aef1f9046baa2f949015be6f5cf3
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-03 04:34:26 +0900

    Time: 99 ms (58.64%), Space: 28.3 MB (25.09%) - LeetHub

--- LeetCode/Medium/0454-4sum-ii/0454-4sum-ii.cpp
+++ LeetCode/Medium/0454-4sum-ii/0454-4sum-ii.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int N = nums1.size();
-        vector<int> sums(N*N);
+        unordered_map<int, int> um;
         int ans = 0;
 
         int idx = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                sums[idx++] = nums3[i] + nums4[j];
+                um[nums3[i] + nums4[j]]++;
             }
         }
-
-        sort(sums.begin(), sums.end());
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 int sum = nums1[i] + nums2[j];
-                auto [it1, it2] = equal_range(sums.begin(), sums.end(), -sum);
-
-                ans += it2 - it1;
+                
+                ans += um[-sum];
             }
         }
 
