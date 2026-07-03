20a6328e7bf358bcebe737a343659fd0dc96b6d1
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-04 01:43:47 +0900

    Time: 0 ms (100%), Space: 11.3 MB (9.14%) - LeetHub

--- LeetCode/Easy/1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ LeetCode/Easy/1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    bool check(vector<int>& nums, int mid){
+        auto it = lower_bound(nums.begin(), nums.end(), mid);
+
+        return nums.end() - it >= mid;
+    }
+
     int specialArray(vector<int>& nums) {
-        int cnt[1'005] = {0, };
-        for(auto& num: nums) cnt[num]++;
+        sort(nums.begin(), nums.end());
 
-        for(int i = 999; i>=0; i--){
-            cnt[i] += cnt[i+1];
+        // TTTTTFFFFF
+        // x 이상인 수가 x 이상개 존재해야 함.
+
+        int yes = 0, no = 1'001;
+        while(yes + 1 < no){
+            int mid = (yes + no)/2;
+
+            if(check(nums, mid)) yes = mid;
+            else no = mid;
         }
 
-        for(int i = 0; i <= 1'000; i++){
-            if(cnt[i] < i) break;
+        auto it = lower_bound(nums.begin(), nums.end(), yes);
 
-            if(cnt[i] == i) return i;
-        }
-
-        return -1;
+        return nums.end() - it == yes ? yes : -1;
     }
 };
