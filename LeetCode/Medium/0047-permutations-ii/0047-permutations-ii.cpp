f1306406153a88d936c687e3f7289476e1a1dc33
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-05 23:30:50 +0900

    Time: 0 ms (100%), Space: 12 MB (60.93%) - LeetHub

--- LeetCode/Medium/0047-permutations-ii/0047-permutations-ii.cpp
+++ LeetCode/Medium/0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
+    bool visit[10];
+    vector<int> buffer;
+    vector<vector<int>> ans;
+
+    void backtrack(int k, const vector<int>& nums){
+        if(k == nums.size()){
+            ans.push_back(buffer);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(visit[i]) continue;
+            if(i != 0 && nums[i-1] == nums[i] && !visit[i-1]) continue;
+
+            visit[i] = true;
+            buffer.push_back(nums[i]);
+            backtrack(k+1, nums);
+
+            buffer.pop_back();
+            visit[i] = false;
+        }
+    }
+
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
+        ans.clear();
+        buffer.clear();
         int N = nums.size();
         
         sort(nums.begin(), nums.end());
-
-        do{
-            ans.push_back(nums);
-        }while(next_permutation(nums.begin(), nums.end()));
-
+        fill(visit, visit + N, false);
+        
+        backtrack(0, nums);
 
         return ans;
     }
