2e7a7acc197f03f80a48ac33a7286af9256940b0
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-11 01:50:46 +0900

    Time: 147 ms (57.67%), Space: 107.9 MB (75%) - LeetHub

---
+++ LeetCode/Medium/3462-maximum-sum-with-at-most-k-elements/3462-maximum-sum-with-at-most-k-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        priority_queue<int> pq;
+
+        int N = grid.size();
+        for(int i = 0; i < N; i++)
+            sort(grid[i].begin(), grid[i].end(), greater<int>());
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < limits[i]; j++)
+                pq.push(grid[i][j]);
+        }
+        
+
+        long long ans = 0;
+
+        while(k--){
+            auto cur = pq.top();
+            pq.pop();
+
+            ans += cur;
+        }
+
+        return ans;
+    }
+};
