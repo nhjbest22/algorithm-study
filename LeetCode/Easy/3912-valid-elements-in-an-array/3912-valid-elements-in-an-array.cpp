da1e4fb4d797fbf32de374b1597de99a8d293386
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-11 01:44:21 +0900

    Time: 4 ms (18.56%), Space: 32.4 MB (38.42%) - LeetHub

--- LeetCode/Easy/3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.cpp
+++ LeetCode/Easy/3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     vector<int> findValidElements(vector<int>& nums) {
         int N = nums.size();
-        vector<int> suffix(N, 0), prefix(N, 0);
+        vector<int> suffix(N, 0);
         vector<int> ans;
 
-        prefix[0] = nums[0];
         suffix[N-1] = nums[N-1];
-
-        for(int i = 1; i < N; i++)
-            prefix[i] = max(prefix[i-1], nums[i]);
-        
         for(int i = N-2; i>= 0; i--)
             suffix[i] = max(suffix[i+1], nums[i]);
+
+        int MAX = -1;
         
         for(int i = 0; i < N; i++){
             if(i == 0 || i == N-1){
                 ans.push_back(nums[i]);
+                MAX = max(nums[i], MAX);
+
                 continue;
             }
 
-            if(nums[i] > prefix[i-1] || nums[i] > suffix[i+1])
+
+            if(MAX < nums[i] || nums[i] > suffix[i+1])
                 ans.push_back(nums[i]);
             
+            MAX = max(MAX, nums[i]);
         }
 
         return ans;
