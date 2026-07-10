9edfe7a47758a0d935ce49bc328762026712b32d
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-11 02:47:32 +0900

    Time: 7 ms (70.36%), Space: 30.7 MB (43.29%) - LeetHub

---
+++ LeetCode/Medium/0436-find-right-interval/0436-find-right-interval.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<pair<int, int>> starts, ends;
+        int N = intervals.size();
+        vector<int> ans(N, -1);
+
+        for(int i = 0; i < N; i++){
+            auto& interval = intervals[i];
+            starts.push_back({interval[0], i});
+            ends.push_back({interval[1], i});
+        }
+
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        int idx = 0;
+        for(int i = 0; i < N; i++){
+            auto& en = ends[i];
+            
+            while(idx < N && en.first > starts[idx].first)
+                idx++;
+
+            if(idx == N) break;
+            
+            ans[en.second] = starts[idx].second;
+        }
+
+        return ans;
+    }
+};
