d64b6e8e74c3d3a09a9f7636c7ca123a129ccc4d
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-11 03:14:39 +0900

    Time: 28 ms (67.06%), Space: 107.7 MB (26.38%) - LeetHub

--- LeetCode/Medium/2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ LeetCode/Medium/2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -12,17 +12,7 @@ public:
         for(auto& [key, value]: um){
             if(value == 1) return -1;
 
-            int cnt = value / 3;
-            int remainder = value % 3;
-
-            if(remainder % 2 != 0){
-                cnt--;
-                remainder += 3;
-            }
-
-            cnt += remainder / 2;
-
-            ans += cnt;
+            ans += (value + 2) / 3;
         }
 
         return ans;
