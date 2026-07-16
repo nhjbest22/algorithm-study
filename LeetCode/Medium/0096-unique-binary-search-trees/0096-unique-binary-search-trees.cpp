b9a987320eb81ea6786779d7334f8b77057569f3
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-17 01:22:24 +0900

    Time: 0 ms (100%), Space: 7.8 MB (89.77%) - LeetHub

--- LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int numTrees(int n) {
-        // 1 -> 
-        // 2 -> 2
-        // 3 -> 5
-        // 4 -> 5 + 3 + 3 + 5 -> 16
         int dp[20];
         dp[0] = 1; dp[1] = 1; 
         dp[2] = 2; dp[3] = 5;
