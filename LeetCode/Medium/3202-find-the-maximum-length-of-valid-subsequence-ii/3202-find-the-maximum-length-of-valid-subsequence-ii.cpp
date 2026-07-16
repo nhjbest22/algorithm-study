d1aa6aa02b4f3cc386d00be80368a338c6ac5f4f
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-16 21:38:29 +0900

    Time: 123 ms (55.88%), Space: 32.2 MB (93.02%) - LeetHub

--- LeetCode/Medium/3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ LeetCode/Medium/3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -4,19 +4,18 @@ public:
         int N = nums.size();
         for(auto& num: nums)
             num %= k;
+        
+        int dp[1005][1005] = {0, }; // (마지막 값, 목표 합)
 
         int MAX = -1;
-        int dp[k + 5];
 
-        for(int C = 0; C < k; C++){
-            fill(dp, dp + k, 0);
+        for(auto& num: nums){
+            for(int mod = 0; mod < k; mod++){
+                int target = (mod - num + k) % k;
+                dp[num][mod] = max(dp[num][mod], dp[target][mod] + 1);
 
-            for(auto& num: nums){
-                int target = (C - num + k) % k;
-                dp[num] = dp[target] + 1;
+                MAX = max(MAX, dp[num][mod]);
             }
-
-            MAX = max(MAX, *max_element(dp, dp + k));
         }
 
         return MAX;
