1ed5b049693ccf736966b707d8205efdd0e815b5
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-18 19:39:09 +0900

    Time: 16 ms (32.27%), Space: 176.9 MB (77.64%) - LeetHub

---
+++ LeetCode/Medium/2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> colorTheArray(int n, vector<vector<int>>& queries) {
+        int M = queries.size();
+        
+        vector<int> colors(n, 0);
+        vector<int> ans(M, 0);
+
+        for(int i = 0; i < M; i++){
+            if(i) ans[i] = ans[i-1];
+
+            auto& q = queries[i];
+            int idx = q[0], C = q[1];
+            
+            if(colors[idx] != 0){
+                if(idx - 1 >= 0 && colors[idx-1] == colors[idx]) ans[i]--;
+                if(idx + 1 < n && colors[idx] == colors[idx + 1]) ans[i]--;
+            }
+
+            colors[idx] = C;
+
+            if(idx - 1 >= 0 && colors[idx-1] == colors[idx]) ans[i]++;
+            if(idx + 1 < n && colors[idx] == colors[idx + 1]) ans[i]++;
+        }
+
+        return ans;
+    }
+};
