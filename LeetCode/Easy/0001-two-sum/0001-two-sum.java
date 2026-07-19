ef97a1b6a2aaf23e9b8a307374ae3a8715ee167a
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-19 20:46:16 +0900

    Time: 2 ms (99.3%), Space: 47.2 MB (23.34%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,26 +1,18 @@
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        record Pair(int num, int idx) {}
         int N = nums.length;
-        Pair[] arr = new Pair[N];
+        Map<Integer, Integer> m = new HashMap<>();
 
-        for(int i = 0; i < N; i++)
-            arr[i] = new Pair(nums[i], i);
-        
-        Arrays.sort(arr, (a, b) -> a.num() - b.num());
-        int l = 0, r = N - 1;
+        for(int i = 0; i < N; i++){
+            int find = target - nums[i];
 
-        while(l < r){
-            int sum = arr[l].num() + arr[r].num();
+            if(m.get(find) != null)
+                return new int[]{m.get(find), i};
 
-            if(sum == target) break;
-
-            if(sum < target) l++;
-            else r--;
+            m.put(nums[i], i);
         }
 
-        int[] ans = {arr[l].idx(), arr[r].idx()};
-        return ans;
+        return new int[]{};
     }
 }
