38a6cdd5f03657ce4994c8d78048780e6ec4b0ff
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-19 21:53:06 +0900

    Time: 6 ms (20.73%), Space: 51.5 MB (83.57%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -3,12 +3,10 @@ class Solution {
     int dy[] = {0, 1, 0, -1};
 
     public int numIslands(char[][] grid) {
-        record Grid(int x, int y) {}
-
         int ans = 0;
         int N = grid.length, M = grid[0].length;
 
-        Queue<Grid> Q = new ArrayDeque<>();
+        Queue<int[]> Q = new ArrayDeque<>();
         
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
@@ -17,11 +15,11 @@ class Solution {
                 ans++;
 
                 grid[i][j] = '0';
-                Q.offer(new Grid(i, j));
+                Q.offer(new int[]{i, j});
 
                 while(!Q.isEmpty()){
-                    Grid cur = Q.poll();
-                    int curX = cur.x(), curY = cur.y();
+                    int[] cur = Q.poll();
+                    int curX = cur[0], curY = cur[1];
 
                     for(int dir = 0; dir < 4; dir++){
                         int nxtX = curX + dx[dir];
@@ -31,7 +29,7 @@ class Solution {
                         if(grid[nxtX][nxtY] == '0') continue;
 
                         grid[nxtX][nxtY] = '0';
-                        Q.offer(new Grid(nxtX, nxtY));
+                        Q.offer(new int[]{nxtX, nxtY});
                     }
                 }
 
