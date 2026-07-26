4008ca9fc5c070217c1ac67d02f8dafc0e8bcc11
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-26 11:38:12 +0900

    Time: 22 ms (88.6%), Space: 51.8 MB (92.31%) - LeetHub

--- LeetCode/Medium/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ LeetCode/Medium/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int[][] ans = new int[k][2];
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            int dist1 = a[0]*a[0] + a[1]*a[1];
-            int dist2 = b[0]*b[0] + b[1]*b[1];
+        int N = points.length;
 
-            return dist2 - dist1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->{
+            int d1 = a[0]*a[0] + a[1]*a[1];
+            int d2 = b[0]*b[0] + b[1]*b[1];
+
+            return d1 - d2;
         });
 
-        int N = points.length;
-        for(int[] p: points){
-            pq.offer(p);
-
-            if(pq.size() > k)
-                pq.poll();
-        }    
+        for(int i = 0; i < N; i++)
+            pq.offer(points[i]);
 
         for(int i = 0; i < k; i++)
             ans[i] = pq.poll();
-        
 
         return ans;
     }
