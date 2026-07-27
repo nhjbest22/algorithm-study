b5865c1246d39542dfa8ea8f64a4591d75bd20f1
Seungmin Nam <nhjbest22@g.skku.edu>
2026-07-27 22:23:51 +0900

    Time: 13 ms (88.67%), Space: 37.6 MB (37.42%) - LeetHub

--- LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<int> v1, v2;
-        unordered_set<int> us1, us2;
+        unordered_set<int> us1(nums1.begin(), nums1.end());
+        unordered_set<int> us2(nums2.begin(), nums2.end());
 
-        for(int i = 0; i < nums1.size(); i++)
-            us1.insert(nums1[i]);
-        
-        for(int i = 0; i < nums2.size(); i++)
-            us2.insert(nums2[i]);
-        
         for(auto& num: us1){
             if(us2.find(num) != us2.end())
                 continue;
