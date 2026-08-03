dfc6d5ae6a6aac42761b5c56666b932cfd9beb01
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-03 23:41:59 +0900

    Time: 34 ms (93.35%), Space: 25.1 MB (81.96%) - LeetHub

--- LeetCode/Medium/3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ LeetCode/Medium/3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int maxTotalReward(vector<int>& reward) {
-        bool dp[2005][4005] = {false, };
-        sort(reward.begin(), reward.end());
-        dp[0][reward[0]] = true;
-
-        int MAX = reward[0];
-
         int N = reward.size();
-        for(int i = 1; i < N; i++){
+        sort(reward.begin(), reward.end());
+
+        bool dp[4005] = {false, };
+        dp[0] = true;
+        
+        for(int i = 0; i < N; i++){
             int& cur = reward[i];
-            int& prev = reward[i-1];
-            dp[i][cur] = true;
-
-            for(int j = 0; j< 2*prev; j++){
-                if(!dp[i-1][j]) continue;
-
-                dp[i][j] = true;
-                if(j < cur) dp[i][j + cur] = true;
+            for(int j = 2*cur -1; j >= cur; j--){
+                if(dp[j-cur])
+                    dp[j] = true;
             }
         }
 
-        int& en = reward[N-1];
-        for(int i = en; i < 2*en; i++)
-            if(dp[N-1][i]) 
-                MAX = i;
+        for(int i = 4004; i >= 0; i--)
+            if(dp[i]) return i;
         
-        return MAX;
+        return 0;
     }
 };
