a7f8c8e64ceee7ca9785d780b1a2f29421b07e9b
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-03 23:59:00 +0900

    Time: 2109 ms (16.46%), Space: 58.2 MB (35.44%) - LeetHub

---
+++ LeetCode/Hard/3181-maximum-total-reward-using-operations-ii/3181-maximum-total-reward-using-operations-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& reward) {
+        int N = reward.size();
+        sort(reward.begin(), reward.end());
+        const int SZ = 100001;
+
+        bitset<SZ> dp, ones;
+        dp[0] = 1;
+        ones.set();
+
+        for(int& cur: reward){
+            bitset<SZ> mask = ones >> (SZ - cur);
+            dp |= (dp & mask) << cur;
+        }
+
+        for(int i = SZ; i >= 0; i--)
+            if(dp[i]) return i;
+
+        return 0;
+    }
+};
