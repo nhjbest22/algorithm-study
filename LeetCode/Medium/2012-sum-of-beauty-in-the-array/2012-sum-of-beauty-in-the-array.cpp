c908af82beb651f5b85204df43f6528bba096b76
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-08 16:29:00 +0900

    Time: 3 ms (75.05%), Space: 89.8 MB (72.84%) - LeetHub

--- LeetCode/Medium/2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ LeetCode/Medium/2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
         int N = nums.size();
-        vector<int> prefix(N, 0), suffix(N, 0);
-        vector<int> state(N, 0);
+        int ans = 0;
+        vector<int> suffix(N, 0);
 
-        // prefix -> MAX, suffix -> MIN
-
-        prefix[0] = nums[0];
-        for(int i = 1; i < N; i++)
-            prefix[i] = max(nums[i], prefix[i-1]);
-        
         suffix[N-1] = nums[N-1];
         for(int i = N-2; i>=0; i--)
             suffix[i] = min(nums[i], suffix[i+1]);
 
+        int left_max = nums[0];
         for(int i = 1; i < N-1; i++){
+            left_max = max(left_max, nums[i-1]);
             int& num = nums[i];
 
-            if(num > prefix[i-1] && num < suffix[i+1]){
-                state[i] = 2;
+            if(num > left_max && num < suffix[i+1]){
+                ans += 2;
                 continue;
             }
 
             if(num > nums[i-1] && num < nums[i+1])
-                state[i] = 1;
+                ans += 1;
         }
-
-        int ans = accumulate(state.begin(), state.end(), 0);
+        
         return ans;
     }
 };
