849a9ad44b2234b058ee5023145902e50acb4c4c
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-14 18:57:37 +0900

    Time: 63 ms (62.3%), Space: 43.8 MB (93.76%) - LeetHub

--- LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,41 +9,17 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*> um_par;
-    int l_h, r_h;
+    TreeNode* dfs(TreeNode* cur, TreeNode* p, TreeNode* q){
+        if(!cur || cur == p || cur == q) return cur;
 
-    void dfs(TreeNode* cur, TreeNode* pre, int height, TreeNode* p, TreeNode* q){
-        if(!cur) return;
+        auto l = dfs(cur->left, p ,q);
+        auto r = dfs(cur->right, p ,q);
 
-        um_par[cur] = pre;
-
-        if(p == cur) l_h = height;
-        if(q == cur) r_h = height;
-
-        dfs(cur->left, cur, height + 1, p, q);
-        dfs(cur->right, cur, height + 1, p, q);
+        if(l && r) return cur;
+        return l ? l : r;
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        um_par.clear();
-
-        dfs(root, root, 0, p, q);
-
-        if(l_h < r_h){
-            swap(p, q);
-            swap(l_h, r_h);
-        }
-
-        while(l_h != r_h){
-            p = um_par[p];
-            l_h--;
-        }
-
-        while(p != q){
-            p = um_par[p];
-            q = um_par[q];
-        }
-
-        return p;
+        return dfs(root, p, q);
     }
 };
