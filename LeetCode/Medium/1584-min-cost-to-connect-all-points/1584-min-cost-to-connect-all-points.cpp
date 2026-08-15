76f4808c7447b7d7a27d06a4f04315a2d108ec0c
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-16 03:20:17 +0900

    Time: 199 ms (68.98%), Space: 61.9 MB (56.42%) - LeetHub

---
+++ LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public: 
+    int p[1'005];
+    
+    int find(int x){
+        if(p[x] < 0) return x;
+
+        return p[x] = find(p[x]);
+    }
+
+    bool unite(int u, int v){
+        u = find(u);
+        v = find(v);
+
+        if(u == v) return false;
+        if(p[u] == p[v]) p[u]--;
+
+        if(p[u] < p[v]) p[v] = u;
+        else p[u] = v;
+
+        return true;
+    }
+
+    int cost(vector<int>& p1, vector<int>& p2){
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int N = points.size();
+        fill(p, p + N, -1);
+
+        vector<tuple<int, int, int>> edges;
+        for(int i = 0; i < N; i++){
+            for(int j= i+1; j < N; j++){
+                edges.push_back({cost(points[i], points[j]), i, j});
+            }
+        }
+
+        sort(edges.begin(), edges.end());
+
+        int cnt = 0, ans = 0;
+        for(auto& [c, u, v]: edges){
+            if(!unite(u, v)) continue;
+
+            cnt++;
+            ans += c;
+            if(cnt == N-1) break;
+        }
+
+        return ans;
+    }
+};
