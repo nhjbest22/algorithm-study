b4b3209ee55434a78d1dc5380d3fb0bec83bb853
Seungmin Nam <nhjbest22@g.skku.edu>
2026-08-16 01:04:55 +0900

    Time: 544 ms (23.47%), Space: 256.2 MB (56.48%) - LeetHub

--- LeetCode/Medium/3608-minimum-time-for-k-connected-components/3608-minimum-time-for-k-connected-components.cpp
+++ LeetCode/Medium/3608-minimum-time-for-k-connected-components/3608-minimum-time-for-k-connected-components.cpp
@@ -2,51 +2,57 @@
 
 class Solution {
 public:
-    vector<pair<int, int>> adj[100'005]; // cost, nxt
-    bool visit[100'005];
-    
-    void dfs(int cur, int mid){
-        for(auto& [cost, nxt]: adj[cur]){
-            if(cost <= mid) continue;
-            if(visit[nxt]) continue;
+    int p[100'005];
+    vector<pair<int, int>> adj[100'005]; //cost, nxt
 
-            visit[nxt] = true;
-            dfs(nxt, mid);
-        }
+    int find(int x){
+        if(p[x] < 0) return x;
+
+        return p[x] = find(p[x]);
     }
 
-    bool check(int mid, int k, int n){
-        int cnt = 0;
+    bool is_diff_group(int u, int v){
+        u = find(u);
+        v = find(v);
 
-        for(int i = 0; i < n; i++){
-            if(visit[i]) continue;
+        if(u == v) return false;
+        if(p[u] == p[v]) p[u]--;
 
-            cnt++;
-            visit[i] = true;
-            dfs(i, mid);
-        }
+        if(p[u] < p[v]) p[v] = u;
+        else p[u] = v;
 
-        return cnt >= k;
+        return true;
     }
 
     int minTime(int n, vector<vector<int>>& edges, int k) {
-        int yes = 1e9 + 1, no = -1; // connnected components가 k개 이상이 되도록 하는 최소
         for(int i = 0; i < n; i++) adj[i].clear();
 
         for(auto& e: edges){
             int u = e[0], v = e[1], c = e[2];
+
             adj[u].push_back({c, v});
             adj[v].push_back({c, u});
         }
+        fill(p, p + n, -1);
+        sort(edges.begin(), edges.end(), [&](const auto& a, const auto& b){
+            return a[2] > b[2];
+        });
+        
+        int cnt = n;
+        int m = edges.size();
+        for(int i = 0; i < m;){
+            int c = edges[i][2];
+            int j = i;
 
-        while(yes > no + 1){
-            int mid = (yes + no)/2;
-            fill(visit, visit + n, false);
+            while(j < m && edges[j][2] == c){
+                if(is_diff_group(edges[j][0], edges[j][1])) cnt--;
+                j++;
+            }
 
-            if(check(mid, k, n)) yes = mid;
-            else no = mid;
+            if(cnt < k) return c;
+            i = j;
         }
 
-        return yes;
+        return 0;
     }
 };
