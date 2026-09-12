7e4ad8f4455b31e49b74b0ce4d7c8a9e877f0368
Seungmin Nam <nhjbest22@g.skku.edu>
2026-09-13 01:21:50 +0900

    Time: 7 ms (93.79%), Space: 33.5 MB (97.25%) - LeetHub

---
+++ LeetCode/Medium/0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int M = mat.size(), N = mat[0].size();
+        vector<vector<int>> dist(M, vector<int>(N, 0));
+
+        queue<pair<int ,int>> Q;
+
+        for(int i=0; i < M; i++){
+            for(int j=0; j< N; j++){
+                if(mat[i][j]) continue;
+
+                for(int dir = 0; dir < 4; dir++){
+                    int x = i + dx[dir];
+                    int y = j + dy[dir];
+
+                    if(x < 0 || x >= M || y < 0 || y >= N) continue;
+                    if(mat[x][y] == 0 || dist[x][y]) continue;
+
+                    dist[x][y] = 1;
+                    Q.push({x, y});
+                }
+            }
+        }
+
+        while(!Q.empty()){
+            auto [curX, curY] = Q.front();
+            Q.pop();
+
+            for(int dir = 0; dir < 4; dir++){
+                int x = curX + dx[dir];
+                int y = curY + dy[dir];
+
+                if(x < 0 || x >= M || y < 0 || y >= N) continue;
+                if(mat[x][y] == 0 || dist[x][y]) continue;
+
+                dist[x][y] = dist[curX][curY] + 1;
+                Q.push({x, y});
+            }
+        }
+
+        return dist;
+    }
+};
