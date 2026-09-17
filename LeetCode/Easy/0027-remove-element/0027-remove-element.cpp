c000088ba0cc271e2ac606339246993aa2ae4a09
Seungmin Nam <nhjbest22@g.skku.edu>
2026-09-17 22:34:06 +0900

    Time: 0 ms (100%), Space: 11.7 MB (84.22%) - LeetHub

--- LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int N = nums.size();
-        int l = 0, r = N-1;
 
-        while(l <= r){
-            while(r >= 0 && nums[r] == val) r--;
-            while(l < N && nums[l] != val) l++;
+        int idx = 0;
+        for(int i = 0; i < N; i++){
+            if(nums[i] == val) continue;
 
-            if(l > r) break;
-
-            swap(nums[l], nums[r]);
+            nums[idx++] = nums[i];
         }
 
-        return r + 1;
+        return idx;
     }
 };
