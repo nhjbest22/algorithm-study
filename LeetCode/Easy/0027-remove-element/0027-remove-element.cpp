1d7dd7ef52a00cf12fa73872755cc0ed0684626f
Seungmin Nam <nhjbest22@g.skku.edu>
2026-09-17 22:40:21 +0900

    Time: 0 ms (100%), Space: 11.6 MB (84.22%) - LeetHub

--- LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int N = nums.size();
-
-        int idx = 0;
-        for(int x: nums){
-            if(x != val) nums[idx++] = x;
-        }
-
-        return idx;
+        return remove(nums.begin(), nums.end(), val) - nums.begin();
     }
 };
