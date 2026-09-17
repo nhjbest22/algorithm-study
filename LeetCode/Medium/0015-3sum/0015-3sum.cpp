886fcf1a6ab73fe02fd81a7304d3a54853d5a6b2
Seungmin Nam <nhjbest22@g.skku.edu>
2026-09-17 23:25:20 +0900

    Time: 39 ms (94.58%), Space: 29 MB (92.48%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -7,26 +7,24 @@ public:
         sort(nums.begin(), nums.end());
 
         for(int i = 0 ; i < N; i++){
+            if(nums[i] > 0) break;
             if(i > 0 && nums[i] == nums[i-1]) continue;
 
             int l = i + 1, r = N - 1;
             while(l < r){
                 int sum = nums[i] + nums[l] + nums[r];
 
-                if(sum == 0){
+                if(sum < 0) l++;
+                else if(sum > 0) r--;
+                else{
                     ans.push_back({nums[i], nums[l], nums[r]});
 
                     l++;
-                    while(l < N && nums[l-1] == nums[l]) l++;
-
                     r--;
-                    while(r > i && nums[r] == nums[r+1]) r--;
 
-                    continue;
+                    while(l < r && nums[l] == nums[l-1]) l++;
+                    while(l < r && nums[r] == nums[r+1]) r--;
                 }
-                
-                if(sum <= 0) l++;
-                else r--;
             }
         }
 
